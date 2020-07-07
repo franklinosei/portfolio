991338eb870fd89f3f8851391bea6664d299f55a
franklinosei <franklinosei15@gmail.com>
2020-07-07 15:20:31 +0000

    Updates

---
+++ precache-manifest.477229ba95e3f7cd1c05707582fce4c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220dfc14377e95938aa244b0cabfc094",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7524a4709e9929e8d9ea",
+    "url": "/portfolio/static/css/2.be61a909.chunk.css"
+  },
+  {
+    "revision": "4fccc26bc65549af64b5",
+    "url": "/portfolio/static/css/main.827729b6.chunk.css"
+  },
+  {
+    "revision": "7524a4709e9929e8d9ea",
+    "url": "/portfolio/static/js/2.717ca4a8.chunk.js"
+  },
+  {
+    "revision": "7c9358abffa72e99223c879dd381d4b0",
+    "url": "/portfolio/static/js/2.717ca4a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fccc26bc65549af64b5",
+    "url": "/portfolio/static/js/main.0ede3ca6.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
