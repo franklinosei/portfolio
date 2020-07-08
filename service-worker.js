663438ae5af0d0de7c0d918c53d22d7eefc271a9
franklinosei <franklinosei15@gmail.com>
2020-07-08 22:37:46 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4d75d10816db170296bb95610f3535f5.js"
+  "/portfolio/precache-manifest.1b3cfce5fbf0398151bfc667ab736976.js"
 );
 
 self.addEventListener('message', (event) => {
