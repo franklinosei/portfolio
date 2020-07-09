b17cebbd941b3cea6d98d48d9747ffc66d552845
franklinosei <franklinosei15@gmail.com>
2020-07-09 11:11:05 +0000

    Updates

---
+++ precache-manifest.7d6080e948971bb21ee546ee0aa1c291.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1c0e45ee83d31247aa231d51e8cdb9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "89eac5fd30448be69a7f",
+    "url": "/portfolio/static/css/2.be61a909.chunk.css"
+  },
+  {
+    "revision": "f0550e0179d3bf74f4aa",
+    "url": "/portfolio/static/css/main.7edd5738.chunk.css"
+  },
+  {
+    "revision": "89eac5fd30448be69a7f",
+    "url": "/portfolio/static/js/2.7de66e4a.chunk.js"
+  },
+  {
+    "revision": "7c9358abffa72e99223c879dd381d4b0",
+    "url": "/portfolio/static/js/2.7de66e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0550e0179d3bf74f4aa",
+    "url": "/portfolio/static/js/main.5f61183d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "882338a1245aef669b6ffc3e718659fe",
+    "url": "/portfolio/static/media/img.882338a1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1b3cfce5fbf0398151bfc667ab736976.js"
+  "/portfolio/precache-manifest.7d6080e948971bb21ee546ee0aa1c291.js"
 );
 
 self.addEventListener('message', (event) => {
