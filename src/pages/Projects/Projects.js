f53fa47a5469f7b206a63c352f69052ba15709fc
franklin osei <franklinosei15@gmail.com>
2020-08-27 04:53:43 +0000

    Updated the projects page, little touches to go

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import './Projects.css';
+import {
+  Card, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle, Col
+} from 'reactstrap';
 
 const Projects = ({ user }) => {
   const db = [
@@ -8,25 +12,29 @@ const Projects = ({ user }) => {
       id: 1,
       name: 'project name',
       link: 'github link',
-      image: 'https://unsplash.com/photos/qjnAnF0jIGk/download?force=true&w=640'
+      image: 'https://unsplash.com/photos/qjnAnF0jIGk/download?force=true&w=640',
+      description: 'lorenzxjkcndch zjkcbfvcbx hzb zcb zcbybchch'
     },
     {
       id: 2,
       name: 'project name',
       link: 'github link',
-      image: 'https://unsplash.com/photos/qjnAnF0jIGk/download?force=true&w=640'
+      image: 'https://unsplash.com/photos/qjnAnF0jIGk/download?force=true&w=640',
+      description: 'lorenzxjkcndch zjkcbfvcbx hzb zcb zcbybchch'
     },
     {
       id: 3,
       name: 'project name',
       link: 'github link',
-      image: 'https://unsplash.com/photos/qjnAnF0jIGk/download?force=true&w=640'
+      image: 'https://unsplash.com/photos/qjnAnF0jIGk/download?force=true&w=640',
+      description: 'lorenzxjkcndch zjkcbfvcbx hzb zcb zcbybchch'
     },
     {
       id: 4,
       name: 'project name',
       link: 'github link',
-      image: 'https://unsplash.com/photos/qjnAnF0jIGk/download?force=true&w=640'
+      image: 'https://unsplash.com/photos/qjnAnF0jIGk/download?force=true&w=640',
+      description: 'lorenzxjkcndch zjkcbfvcbx hzb zcb zcbybchch'
     }
   ]
 
@@ -40,6 +48,32 @@ const Projects = ({ user }) => {
             <hr className='hr shadow-3'/>  
           </div>
 
+          <div className='project-content'>
+            {
+              db.map((project, index) => {
+                return(
+                  <Col sm>
+                    <div className='cards shadow-3'>
+                      <Card key={index}>
+                        <CardBody>
+                          <CardTitle>{project.name}</CardTitle>
+                          <CardSubtitle>Card subtitle</CardSubtitle>
+                        </CardBody>
+                        <img width="100%" src={project.image} alt={project.name} />
+                        <CardBody>
+                          <CardText>{project.description}</CardText>
+                          <CardLink href={project.link}>Card Link</CardLink>
+                          <CardLink href={project.link}>Another Link</CardLink>
+                        </CardBody>
+                      </Card>
+                    </div>
+                  </Col>
+                )
+              })
+                
+              
+            }
+          </div>
 
          </div>
       </div>      
