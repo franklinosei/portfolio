4ba99abc4f03f40297730b5137e0840a467a2a45
franklin osei <franklinosei15@gmail.com>
2020-08-28 15:51:16 +0000

    .

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import './Projects.css';
-import {
-  Card, CardText, CardBody, CardLink,
-  CardTitle, CardSubtitle, Col
-} from 'reactstrap';
 import {AwesomeButton} from 'react-awesome-button';
-import {AiFillGithub} from 'react-icons/ai';
 
 const Projects = ({ user }) => {
   const db = [
@@ -23,27 +18,13 @@ const Projects = ({ user }) => {
       link: 'github link',
       image: 'https://unsplash.com/photos/qjnAnF0jIGk/download?force=true&w=640',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'
-    },
-    {
-      id: 3,
-      name: 'project name',
-      link: 'github link',
-      image: 'https://unsplash.com/photos/qjnAnF0jIGk/download?force=true&w=640',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'
-    },
-    {
-      id: 4,
-      name: 'project name',
-      link: 'google.com',
-      image: 'https://unsplash.com/photos/qjnAnF0jIGk/download?force=true&w=640',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'
     }
   ]
 
   return (
     <Layout user={user}>
       <div className='project-page-wrapper'>
-        <div className='project-area padding-top'>
+        <div className='padding-top'>
 
           <div className='sectionTitle'>
             <h2>Projects</h2>  
@@ -51,39 +32,34 @@ const Projects = ({ user }) => {
           </div>
 
           <div className='project-content'>
+           <div className='project-area'>
             {
               db.map((project, index) => {
                 return(
-                  <Col sm>
-                    <div className='cards shadow-3'>
-                      <Card key={index}>
-                        <CardBody>
-                          <CardTitle><h5 className='cardColor'>{project.name}</h5></CardTitle>
-                          <CardSubtitle>Card subtitle</CardSubtitle>
-                        </CardBody>
-                         <img width="100%" src={project.image} alt={project.name} />
-                        <CardBody>
-                          <CardText><h5 className='cardColor'>{project.description}</h5></CardText>
-                          <CardLink href={project.link}>
-                            <AwesomeButton size="medium" type="primary"> Card Link </AwesomeButton>
-                          </CardLink>
-                          <CardLink href={project.link}>
-                            <AwesomeButton size="medium" type="primary">
-                              <AiFillGithub /> Code
-                            </AwesomeButton>
-                          </CardLink>
-                        </CardBody>
-                      </Card>
-                    </div>
-                  </Col>
+                  
+                      <div className='cards shadow-3 br3'>
+                        <h5>{project.name}</h5>
+                        <div className="card-image">
+                            <img src={project.image} alt={project.name} />
+                        </div>
+                        <br /> 
+                        <div className="content">
+                          <p>{project.description}</p>
+                        </div> 
+                        <a href={'"' + project.link + '"'}>
+                          <AwesomeButton size="medium" type="primary">
+                             See it
+                          </AwesomeButton>
+                        </a>
+                      </div>
+                   
                 )
-              })
-                
-              
+              })                       
             }
+            </div>
           </div>
 
-         </div>
+        </div>
       </div>      
 
     </Layout>
