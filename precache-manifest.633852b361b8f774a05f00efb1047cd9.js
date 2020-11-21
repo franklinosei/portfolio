0a45c927b425829108128f1c18337fddcb7075f7
Franklin Osei <47639574+franklinosei@users.noreply.github.com>
2020-11-21 15:03:09 +0000

    Add files via upload

---
+++ precache-manifest.633852b361b8f774a05f00efb1047cd9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8954c6e1bcac2b06b20649e7275be4c0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "abac7985918f62fe0a80",
+    "url": "/portfolio/static/css/2.87a1bd6a.chunk.css"
+  },
+  {
+    "revision": "031067d6e7b2aba9e823",
+    "url": "/portfolio/static/css/main.e2656511.chunk.css"
+  },
+  {
+    "revision": "abac7985918f62fe0a80",
+    "url": "/portfolio/static/js/2.be41bd2e.chunk.js"
+  },
+  {
+    "revision": "7c9358abffa72e99223c879dd381d4b0",
+    "url": "/portfolio/static/js/2.be41bd2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "031067d6e7b2aba9e823",
+    "url": "/portfolio/static/js/main.dcda34d7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "584003e8c82622b168be8e8f3133e516",
+    "url": "/portfolio/static/media/bg.584003e8.gif"
+  },
+  {
+    "revision": "882338a1245aef669b6ffc3e718659fe",
+    "url": "/portfolio/static/media/img.882338a1.jpg"
+  }
+]);
